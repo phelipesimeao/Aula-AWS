e8d2060727341b1fb601afef83b4847f01057d3c
Phelipe Soares Simeão <phelipe.simeao07@gmail.com>
2026-06-09 20:00:00 -0300

    1st commit

---
+++ AulaAws01/src/main/java/com/AulaoAws/AulaAws01/AulaAws01Application.java
@@ -0,0 +1,13 @@
+package com.AulaoAws.AulaAws01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaAws01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaAws01Application.class, args);
+	}
+
+}
---
+++ AulaAws01/src/test/java/com/AulaoAws/AulaAws01/AulaAws01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AulaoAws.AulaAws01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaAws01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
